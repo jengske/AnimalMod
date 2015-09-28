b7b864353a89e76b83356ae2b152a75815ceaa41
Dirk Peeters <ponystal-peeters@telenet.be>
2015-09-28 22:29:12 +0200

    Update AnimalMod.lua

--- lua/AnimalMod.lua
+++ lua/AnimalMod.lua
@@ -39,9 +39,9 @@ local function letsStart() -- load all files needed by the AnimalMod
 	
 		numFilesLoaded = numFilesLoaded + 1;
 	end;
-	
+	print(AnimalMod.header); -- load in the header for the project only once, all info about the project sits in this file
 	print(('### AnimalMod: initialized %d/%d files (v%s)'):format(numFilesLoaded, numFilesToLoad, AnimalMod.version));
-  print(AnimalMod.header); -- load in the header for the project only once, all info about the project sits in this file
+ 	print("-------------------------------------------------------------------------------------------------------------------------------------------------------");
 end;
 
 letsStart();
