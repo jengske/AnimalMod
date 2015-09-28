042a5db8a69cd7248313b34ff72568ee2614e1a6
Dirk Peeters <ponystal-peeters@telenet.be>
2015-09-29 00:49:56 +0200

    Update database.lua

--- lua/database.lua
+++ lua/database.lua
@@ -1,7 +1,9 @@
 -- this is our main structure for the AnimalMod not the animals itself.
 -- all events, animaltypes and so on need to be defined in these tables
-
+-- file is under construction
+-- do not modify
 local a = AnimalMod; -- just to shorten code
+local g = g_currentMission.husbandries;
 -- stats -- to be synced with our general structure
 a.stats = {}; -- table to store information about the mod
 a.stats.storages = {}; -- info about our storages
@@ -10,16 +12,29 @@ a.stats.animalAreas = {}; -- info about the animal areas defined in the map
 a.stats.triggers = {}; -- info about the animal areas defined in the map, not the animals it self
 a.stats.players = {}; -- info about our players
 
--- general structure to keep track of all --
+-- general table structure to keep track of all --
 a.storages = {}; -- info about our storages /storage.lua 
 a.animals = {}; -- info about our animals / animalType.lua
-a.animalAreas = {}; -- info about the animal areas defined in the map / animalArea.lua
+a.husbandries = g;
+a.animals.g_animals = {}; -- game animals
+a.animals.f_animals = {}; -- fillType animals
+a.animals.o_animals = {}; -- objectType animals
+a.animals.v_animals = {}; -- vehicleType animals
+a.animals.p_animals = {}; -- placeable animals
+
 a.triggers = {}; -- info about the animal areas defined in the map, not the animals it self /AnimalModTrigger.lua
 a.players = {}; -- info about our players
 a.allowedFeedTypes = {}; -- allowed feed types
 a.production = {}; 
 
--- gui --
+-- map stuff --
+a.spawnPlaces = {};
+a.eatPlaces = {};
+a.breedPlaces = {};
+a.animalAreas = {}; -- info about the animal areas defined in the map / animalArea.lua
+a.splines = {}; 
+
+-- gui -- no scripts made just prepairing
 a.gui = {}; -- gui holder
 a.gui.buttons = {}; -- buttons
 a.gui.pages = {}; -- silo's, animals, triggers, ......
