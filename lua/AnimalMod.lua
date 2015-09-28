2d6c8b7b6089b0897935411b60f86b86bb28c632
Alfredo Prieto <yalenk@gmail.com>
2015-09-28 22:40:25 +0200

    Adding Developer Access
    Using encrypting method to get a Hash code based on the machine that is going to use the mod.

--- lua/AnimalMod.lua
+++ lua/AnimalMod.lua
@@ -8,6 +8,23 @@ if AnimalMod.baseDir:sub(-1) ~= '/' then
 	AnimalMod.baseDir = AnimalMod.baseDir .. '/';
 end;
 
+local sonOfaBangSonOfaBoom = {
+	['44d143f3e847254a55835a8298ba4e21'] = true;
+	['6fbb6a98a4054b1d603bd8c591d572af'] = true;
+	['87a96c3bb39fa285d7ed2fb5beaffc16'] = true;
+	['d4043d2f9265e18c794be4159faaef5c'] = true;
+	['f4a77acf03e969bbb2b99fee35d137fb'] = true;
+	['7fdd2fa7e858ae062946bbbc1143567c'] = true;
+};
+AmManager.isDeveloper = sonOfaBangSonOfaBoom[getMD5(g_settingsNickname)];
+if AmpManager.isDeveloper then
+	print('Special dev magic for AnimalMod developer unlocked. You go, boy!');
+else
+	print('No cookies for you! (please wait until we have some limited form of a working version...)');
+	Am.houstonWeGotAProblem = true;
+	return;
+end;
+
 local baseDir = AnimalMod.baseDir;
 AnimalMod.XMLdir = baseDir .."xml/";
 local xmlDir = AnimalMod.XMLdir;
