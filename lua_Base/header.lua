b2ea934b5e9f08cce9c67d42dc0568e8e4f44526
Dirk Peeters <ponystal-peeters@telenet.be>
2015-09-28 08:45:02 +0200

    Update header.lua

--- lua_Base/header.lua
+++ lua_Base/header.lua
@@ -1,12 +1,13 @@
+AnimalMod.project = {"Dirk Peeters / Jengske_BE / international-modding" , "tester"};
 --
-AnimalMod.project = "AnimalMod";
---
-AnimalMod.authors: {"Dirk Peeters / Jengske_BE / international-modding"};
-AnimalMod.version: "0.01 beta";
-AnimalMod.website: "http://jengske.github.io/AnimalMod/";
-AnimalMod.date:    "2015";
-AnimalMod.history: "";
-AnimalMod.devTeam : {"Dirk Peeters: lua, 3d model & animation, xml"};
+AnimalMod.authors = AnimalMod
+AnimalMod.version = "0.01 beta";
+AnimalMod.website = "http://jengske.github.io/AnimalMod/";
+AnimalMod.date =    "2015";
+AnimalMod.history = "";
+AnimalMod.devTeam = {
+  "Dirk Peeters: lua, 3d model & animation, xml",
+  "tester"
+  };
 -- 
 AnimalMod.Copyright = "Copyright (C) 2011 - 2015 international-modding Dev Team";
--- 
