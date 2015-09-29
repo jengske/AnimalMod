15b15b86bca35b9aa13805f550a9efca269bdb65
Dirk Peeters <ponystal-peeters@telenet.be>
2015-09-29 16:43:25 +0200

    Update Utils.lua

--- lua/Utils.lua
+++ lua/Utils.lua
@@ -48,32 +48,6 @@ function AnimalMod.Utils:inheritsFrom( baseClass )
     return new_class
 end;
 
--- to do: sound system: create a pool for sounds to be used during gameplay
--- we go use the pools to load sounds to the animals from out the AnimalMod/sound/animalType root.
--- idea is to also load sounds on animals where there is no sound defined on the animal.zip.
--- maby we go create a AnimalMod.soundUtils for handle sound creations, this i must see during development
--- the utils files will be evaluated
-AnimalMod.animal.soundPools = {};
-AnimalMod.animal.soundPoolsUsed = {};
 
-function AnimalMod.Utils.addSound(animalType, fileName, volume, isplaying )
-    if(AnimalMod.animal.sounds[animalType] == nil) then
-        AnimalMod.animal.sounds[animalType] = animalType;
-    end;
-end;
-
-function AnimalMod.Utils:createSpool(animalType, amount)
-    local s = "soundPools";
-    if(AnimalMod.animal[soundPools] == not nil)then
-        print("AnimalMod ERROR: We already have a soundpool for this animalType.");
-        return;
-    end;
-    
-    if(AnimalMod.animal[soundPools][animalType] == nil)then
-        AnimalMod.animal[soundPools][animalType] = animalType;
-    else
-        return false;
-    end;
-end;
 
 AnimalMod.Utils:inheritsFrom( AnimalMod );
